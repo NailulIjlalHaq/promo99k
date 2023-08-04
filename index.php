cf6a1374e8c21df862b16a01a0de4270a2123513
NailulIjlalHaq <nailul.ijlal@gmail.com>
2023-08-04 13:11:35 +0700

    first

---
+++ index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Lampu LED - Listrik Asia</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+
+<body>
+    <!-- Page Content-->
+    <div class="container px-4 px-lg-5">
+
+        <!-- Heading Row-->
+        <div class="row gx-4 gx-lg-5 align-items-center my-5">
+            <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="assets/Promo Paket Usaha.svg" alt="..." /></div>
+            <div class="col-lg-5">
+                <h1 class="font-weight-light">LED BULB Berkualitas</h1>
+                <p><b>Masih Beli Lampu di toko Elektro? SUDAH BUKAN JAMANNYA!</b> <br> Selain mahal terkadang barang ber merk tapi kualitas tidak sebagus harapan kita. Kini kami datang dengan memberikan solusi : <br>
+                    <li>Bisa Anda Jual Kembali</li>
+                    <li>Tahan Lama &amp; Awet</li>
+                    <li>Harga Lebih Terjangkau</li>
+                    <li>Bergaransi &amp; SNI</li>
+                </p>
+                <a class="btn btn-danger" target="_blank" href="https://api.whatsapp.com/send/?phone=628113480620&text=Hallo+kak...%0ASaya+MAU+order+LED+BULB%2C+apakah+masih+kebagian+PROMO+LED+99rb+dapat+10+pcs+kak...%3F&type=phone_number&app_absent=0">Beli Sekarang!!!</a>
+            </div>
+        </div>
+
+        <!-- Call to Action-->
+        <div class="card text-white my-5 text-center">
+            <div class="card-header bg-danger">
+                <h1 class="card-title text-white card-title m-0"><b>Kelebihan & Manfaat LED Bulb</b></h1>
+            </div>
+            <div class="card-body bg-primary">
+                <b>------------------------------------</b>
+                <p class="card-text">
+                    <li style="font-size: x-large;">Hemat energi sampai 80% tidak seperti lampu pijar.</li>
+                    <li style="font-size: x-large;">Umur lebih lama sampai 10.000 jam.</li>
+                    <li style="font-size: x-large;">Tidak mengandung merkuri yang berbahaya.</li>
+                    <li style="font-size: x-large;">Menghasilkan cahaya lebih terang (6500k).</li>
+                    <li style="font-size: x-large;">Mudah dipasang atau di aplikasikan.</li>
+                    <li style="font-size: x-large;">Efisien dan Efektif serta hemat listrik.</li>
+                </p>
+            </div>
+        </div>
+
+        <div class="card mb-5">
+            <div class="card-header bg-warning text-center">
+                <h1 class="text-red"><b>PROMO!!!</b></h1>
+            </div>
+            <div class="card-body">
+                <p class="text-center">
+                <h3 class="text-center">Harga Normal</h3>
+                <h2 class="text-center text-danger">Rp. 79.000,-</h2>
+                <h4 class="text-center">untuk 1 Lampu</h4>
+                <br>
+                <h3 class="text-center"><b>------------------------------------</b></h3>
+                <br>
+                <h1 class="text-center"><b>Spesial Promo</b></h1>
+                <h3 class="text-center text-primary">(Spesial Agen)</h3>
+                <h1 class="text-center text-danger">Rp. 99.000,-</h1>
+                <h4 class="text-center"><b>10 Lampu</b></h4>
+                </p>
+                <div class="card text-white my-2 py-4 text-center" style="border: 100pc;">
+                    <div class="card-body">
+                        <h1 class="card-title text-danger">Beli Disini🥰!</h1>
+                        <p class="card-text bg-warning text-danger" style="font-size: xx-large;"><b>Khusus Untuk Pembelian Hari Ini Saja🥰!</b></p>
+                        <a target="_blank" href="https://api.whatsapp.com/send/?phone=628113480620&text=Hallo+kak...%0ASaya+MAU+order+LED+BULB%2C+apakah+masih+kebagian+PROMO+LED+99rb+dapat+10+pcs+kak...%3F&type=phone_number&app_absent=0"><img class="img-fluid rounded mb-4 mb-lg-0" src="assets/buy.png" alt="..." /></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Content Row-->
+
+        <div class="card">
+            <div class="card-header bg-primary text-center mb-2">
+                <h1 class="text-white">PENDAFTARAN AGEN GRATIS UNTUK HARI INI!</h1>
+            </div>
+            <div class="row gx-4 gx-lg-5">
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title text-center">Bukti Pembayaran</h2>
+                            <img class="img-fluid rounded mb-4 mb-lg-0" src="assets/1.png" alt="..." />
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title text-center">Bukti Pembayaran</h2>
+                            <img class="img-fluid rounded mb-4 mb-lg-0" src="assets/2.png" alt="..." />
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title text-center">Bukti Pembayaran</h2>
+                            <img class="img-fluid rounded mb-4 mb-lg-0" src="assets/3.png" alt="..." />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Tambahan -->
+        <div class="card text-white bg-danger my-5 py-4 text-center">
+            <div class="card-body">
+                <img class="img-fluid rounded mb-4" src="assets/Second.svg" alt="..." />
+                <img class="img-fluid rounded mb-4" src="assets/Third.svg" alt="..." />
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Footer-->
+    <footer class="py-5 bg-dark">
+        <div class="container px-4 px-lg-5">
+            <p class="m-0 text-center text-white">Copyright &copy; Lampu LED Bulb 2023</p>
+        </div>
+    </footer>
+
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+</body>
+
+</html>
